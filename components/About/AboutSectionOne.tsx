7c26be6c5d3255142157a35bb744807b3d16a54b
HALIMA-abdul <halushka1421@gmail.com>
2025-03-18 12:23:03 +0300

    update Aboutsection

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -24,8 +24,8 @@ const AboutSectionOne = () => {
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle
-                title="Crafted for Startup, SaaS and Business Sites."
-                paragraph="The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel discussions and roundtables."
+                title="Our mission"
+                paragraph="Swahilipot Hub Foundation is a non-profit organization that meaningfully engages and empowers youth (18 to 35 years) to enable them to unleash their full potential. Based in the coastal city of Mombasa in Kenya, it is regulated by the NGO Coordination Board of Kenya.."
                 mb="44px"
               />
 
@@ -34,16 +34,9 @@ const AboutSectionOne = () => {
                 data-wow-delay=".15s"
               >
                 <div className="mx-[-12px] flex flex-wrap">
+                  
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Premium quality" />
-                    <List text="Tailwind CSS" />
-                    <List text="Use for lifetime" />
-                  </div>
-
-                  <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Next.js" />
-                    <List text="Rich documentation" />
-                    <List text="Developer friendly" />
+                   
                   </div>
                 </div>
               </div>
@@ -54,22 +47,34 @@ const AboutSectionOne = () => {
                 className="wow fadeInUp relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0"
                 data-wow-delay=".2s"
               >
-                <Image
-                  src="/images/about/about-image.svg"
+                  <Image
+                  src="/images/blog/mission.png"
                   alt="about-image"
                   fill
-                  className="drop-shadow-three mx-auto max-w-full dark:hidden dark:drop-shadow-none lg:mr-0"
+                  className="drop-shadow-three mx-auto  max-w-full dark:hidden dark:drop-shadow-none lg:mr-0"
                 />
+               
                 <Image
-                  src="/images/about/about-image-dark.svg"
+                  src="/images/blog/mission.png"
                   alt="about-image"
                   fill
                   className="drop-shadow-three mx-auto hidden max-w-full dark:block dark:drop-shadow-none lg:mr-0"
                 />
+                 {/* Button that opens a new tab for the documentary */}
+            <div className="mt-4 text-center">
+              <a
+                href="https://www.youtube.com/watch?v=bscypssE8Ms" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="inline-block bg-primary text-white px-4 py-2 rounded-md hover:bg-primary-dark"
+              >
+                Watch Our Documentary
+              </a>
               </div>
             </div>
           </div>
         </div>
+      </div>
       </div>
     </section>
   );
