6024dca53887a1519500fee7baea8aeec11efdc3
Moseskerempe <musakerempe422@gmail.com>
2025-03-20 13:56:32 +0300

    added accessibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,34 +4,36 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
+import AccessibilityButton from "@/components/Accessibility/AccessibilityButton";
+import AccessibilityMenu from "@/components/Accessibility/AccessibilityMenu";
+import { AccessibilityProvider } from "@/components/Accessibility/AccessibilityContext";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
+import { Providers } from "./providers";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false); // Function to close the menu
+
   return (
     <html suppressHydrationWarning lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         <Providers>
-          <Header />
-          {children}
-          <Footer />
-          <ScrollToTop />
+          <AccessibilityProvider>
+            <AccessibilityButton onClick={() => setMenuOpen(true)} />
+            {menuOpen && <AccessibilityMenu closeMenu={closeMenu} />}
+            <Header />
+            {children}
+            <Footer />
+            <ScrollToTop />
+          </AccessibilityProvider>
         </Providers>
       </body>
     </html>
   );
 }
-
-import { Providers } from "./providers";
